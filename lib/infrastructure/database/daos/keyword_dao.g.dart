40886a5e577a4ddc9b5cc81482be21de8b006ace
wsm0000 <71727820+wsm0000@users.noreply.github.com>
2026-08-02 18:27:01 +0800

    Add lib/infrastructure/database/daos/keyword_dao.g.dart

---
+++ lib/infrastructure/database/daos/keyword_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'keyword_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$KeywordDaoMixin on DatabaseAccessor<AppDatabase> {
+  $KeywordRulesTable get keywordRules => attachedDatabase.keywordRules;
+}
